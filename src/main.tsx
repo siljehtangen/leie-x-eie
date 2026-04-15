4f24d12bc21889145109c74e9ef2abdb77ac6ba1
Silje Hanø Tangen <siljehtangen@outlook.com>
2026-04-15 23:38:56 +0200

    fix: update main

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import './i18n/index.ts'
 import App from './App.tsx'
 import './App.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
 )
