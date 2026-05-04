22b0bb79b325b09ab25d5c1e7a958352b1d60382
Silje Hanø Tangen <siljehtangen@outlook.com>
2026-05-04 18:09:01 +0200

    fix: update main

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import './i18n/index.ts'
 import App from './App.tsx'
 import './App.css'
+import { ErrorBoundary } from './components/ErrorBoundary.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </StrictMode>,
 )
